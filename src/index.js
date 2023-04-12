db49886054afd8f1a6956624db0eb17a45750b9c
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-12 11:39:45 +0200

    First React Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+import { ReactDOM } from "react";
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return <h1>Hello Oskar</h1>        
+    }
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+);
