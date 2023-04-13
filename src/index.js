1face9e7f0c0d601242d4edaca3692742fa8c02b
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-13 10:53:28 +0200

    Connect two components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import { Header } from "./header";
+import { Main } from "./main";
 
-
-
+function App(){
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Header />
+    <App />
 );
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export class Main extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <img className="main-img"
+                src="https://unsplash.it/400/400"
+                alt="" />
+            </div>
+        )
+    }
+}
