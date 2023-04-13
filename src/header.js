740b0407cf6b31bb6ce9cdbc4ef5afd11a51d114
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-13 10:29:28 +0200

    Import first file with add className

---
+++ src/header.js
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <div className="container">
+            <h1>Write first component with using fucntion</h1>
+            <h2>Multiply tags need to be written in div</h2>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import { Header } from "./header";
 
 
-function Header() {
-    return (
-        <div class="container">
-            <h1>Write first component with using fucntion</h1>
-            <h2>Multiply tags need to be written in div</h2>
-        </div>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
