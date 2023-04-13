5c2dec00be37bd0f97664ac33cee7417f684a6f6
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-13 11:25:53 +0200

    Function to change state on click

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ export class Main extends React.Component {
         super(props)
         this.state = {
             text: "some another text to test props in class components",
-            year: 2023
+            year: 2022
         }
+    }
+    switchYear = () => {
+        this.setState({year: this.state.year + 1});
     }
     render() {
         return (
@@ -14,6 +17,8 @@ export class Main extends React.Component {
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor {this.state.text} incididunt ut labore et dolore magna aliqua. {this.state.year}
                 </p>
+                <button onClick={this.switchYear}>Switch year</button>
+                <br />
                 <img className="main-img"
                     src="https://unsplash.it/400/400"
                     alt="Random pic from  unsplash" />
