09e6d90e09d06f54cd7723bcbd1bc2f5ffd2565a
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-13 10:17:10 +0200

    First function added to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 
 
+function Header() {
+    return (
+        <div class="container">
+            <h1>Write first component with using fucntion</h1>
+            <h2>Multiply tags need to be written in div</h2>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Oskar Kuchta</h1>
+    <Header />
 );
