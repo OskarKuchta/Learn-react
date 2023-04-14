10faa7a41ca04406b537757796daf56a3f33968a
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-14 11:05:49 +0200

    Add event to select

--- src/footer.js
+++ src/footer.js
@@ -7,7 +7,7 @@ function Colors() {
 
     return (<div>
         <label htmlFor="colors">Choose your color:</label>
-        <select name="colors" id="colors">
+        <select onMouseOut={changeColor} name="colors" id="colors">
             <option value="red">Red</option>
             <option value="blue">Blue</option>
             <option value="black">Black</option>
