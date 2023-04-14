3fcc3441985e412f1a71da8f11fe9ebb3ee952ca
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-14 10:24:12 +0200

    Nest component in component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import React from "react"
 
+export function InputName() {
+    const showName = (event) => {
+        event.preventDefault();
+        const myName = document.querySelector("#myName");
+        const textBelow = document.querySelector(".typedName");
+        if(myName.value === ""){
+            alert("You forgot type your name");
+        }
+        else if(myName.value.match(/[^A-Za-z]/g)){
+            alert("Can't add special symbols or number")
+        }
+        else {
+        textBelow.innerText = `Hello ${myName.value} on my page`;
+        }
+    }
+    return (
+        <div>
+            <input id="myName" type="text" placeholder="Type your name" />
+            <p className="typedName"></p>
+            <button onClick={showName}>Save</button>
+        </div>
+    )
+}
 export class Main extends React.Component {
     constructor(props) {
         super(props)
@@ -9,7 +32,7 @@ export class Main extends React.Component {
         }
     }
     switchYear = () => {
-        this.setState({year: this.state.year + 1});
+        this.setState({ year: this.state.year + 1 });
     }
     render() {
         return (
@@ -22,6 +45,7 @@ export class Main extends React.Component {
                 <img className="main-img"
                     src="https://unsplash.it/400/400"
                     alt="Random pic from  unsplash" />
+                <InputName />
             </div>
         )
     }
