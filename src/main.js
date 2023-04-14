4c2d8f7fd9640c24b5b24a1b483ca363f5134f7c
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-14 10:36:48 +0200

    Reorganize component in main

--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
 import React from "react"
 
-export function InputName() {
-    const showName = (event) => {
-        event.preventDefault();
-        const myName = document.querySelector("#myName");
-        const textBelow = document.querySelector(".typedName");
-        if(myName.value === ""){
-            alert("You forgot type your name");
-        }
-        else if(myName.value.match(/[^A-Za-z]/g)){
-            alert("Can't add special symbols or number")
-        }
-        else {
-        textBelow.innerText = `Hello ${myName.value} on my page`;
-        }
-    }
-    return (
-        <div>
-            <input id="myName" type="text" placeholder="Type your name" />
-            <p className="typedName"></p>
-            <button onClick={showName}>Save</button>
-        </div>
-    )
-}
-export class Main extends React.Component {
+class Top extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -45,6 +22,39 @@ export class Main extends React.Component {
                 <img className="main-img"
                     src="https://unsplash.it/400/400"
                     alt="Random pic from  unsplash" />
+            </div>
+        )
+    }
+}
+
+function InputName() {
+    const showName = (event) => {
+        event.preventDefault();
+        const myName = document.querySelector("#myName");
+        const textBelow = document.querySelector(".typedName");
+        if (myName.value === "") {
+            alert("You forgot type your name");
+        }
+        else if (myName.value.match(/[^A-Za-z]/g)) {
+            alert("Can't add special symbols or number")
+        }
+        else {
+            textBelow.innerText = `Hello ${myName.value} on my page`;
+        }
+    }
+    return (
+        <div>
+            <input id="myName" type="text" placeholder="Type your name" />
+            <p className="typedName"></p>
+            <button onClick={showName}>Save</button>
+        </div>
+    )
+}
+export class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <Top />
                 <InputName />
             </div>
         )
