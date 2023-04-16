652cab03f7017ba0a9171862f9cd7caaf0f30b0a
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-16 14:55:53 +0200

    Add function value in another function

--- src/header.js
+++ src/header.js
@@ -1,8 +1,28 @@
+function True(){
+    return <p>If props value have true return TRUE</p>
+}
+function False(){
+    return <p>If props value have false return false</p>
+}
+function TakeBoolean(props){
+    const checkBoolean = props.checkBoolean;
+    return (
+    <>
+    <h2>Sentence can be change with checkBoolean props value</h2>
+    {checkBoolean ? <True /> :<False />}
+    </>
+    )
+    // if(checkBoolean){
+    //     return <True />
+    // }
+    // return <False />  (Second method to check condition and add function value)
+}   
 export function Header(props) {
     return (
         <div className="container">
             <h1>Write first component with using fucntion for {props.name}</h1>
             <h2>Multiply tags need to be written in div in {props.framework}</h2>
+            <TakeBoolean checkBoolean={true}/>
         </div>
     )
 }
