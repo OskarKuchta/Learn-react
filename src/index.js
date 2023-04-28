02e686707c37bc0d5549d87b9e59ef7670ef26bb
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-28 20:39:38 +0200

    Add components and styles for it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./components/css/style.css";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <main>
-        
-    </main>
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+
 )
