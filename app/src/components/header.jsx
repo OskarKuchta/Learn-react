7195306ff1f1f8ad432087d66a189b3b42ebfbe7
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-04-29 21:23:47 +0200

    useState with class component

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -1,8 +1,42 @@
-function Header() {
+import React from "react";
+
+class Greetings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+  showGreetings = () => {
+    const output = document.querySelector(".greetings-output");
+    const typedName = document.querySelector("#nameValue");
+    this.setState({ name: typedName.value != "" ? `Hello ${typedName.value} on my page :D` : ""});
+  };
+  render() {
     return (
-        <header>
-        </header>
-    )
+      <div className="greetings">
+        <input
+          type="text"
+          id="nameValue"
+          pattern="[A-Za-z]"
+          placeholder="Type your name"
+        />
+        <button onClick={this.showGreetings} className="greetings-btn">
+          Login
+        </button>
+        <br />
+        <p className="greetings-output">{this.state.name}</p>
+      </div>
+    );
+  }
 }
 
-export default Header
+function Header() {
+  return (
+    <header>
+      <Greetings />
+    </header>
+  );
+}
+
+export default Header;
