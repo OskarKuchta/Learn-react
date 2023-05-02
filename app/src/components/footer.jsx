2a358af9c59d20589c0900e9bde15090600196e6
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-02 19:39:18 +0200

    State to different components

--- app/src/components/footer.jsx
+++ app/src/components/footer.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
+
+function Btn () {
+    const [count, setCount] = useState(0);
+    const clicked = () => {
+        setCount(count => count + 1)
+    } 
+    return (
+        <button onClick={clicked}>Clicked {count} times</button>
+    )
+}
+
 function Footer() {
     return (
         <footer>
+            <Btn />
+            <br />
+            <Btn />
         </footer>
     )
 }
