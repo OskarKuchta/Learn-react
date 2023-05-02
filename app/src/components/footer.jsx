5ac1460cb62d222814ef6ca4b288fd09388d819b
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-02 19:51:39 +0200

    Sharing state with two components

--- app/src/components/footer.jsx
+++ app/src/components/footer.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function Btn () {
-    const [count, setCount] = useState(0);
-    const clicked = () => {
-        setCount(count => count + 1)
-    } 
-    return (
-        <button onClick={clicked}>Clicked {count} times</button>
-    )
+function Btn() {
+  const [count, setCount] = useState(0);
+  const clicked = () => {
+    setCount((count) => count + 1);
+  };
+  return (<>
+  <button onClick={clicked}>Clicked {count} times</button>
+  <br />
+  </>
+  );
 }
 
+function SharingState({ count, onClick }) {
+  return (
+    <>
+      <button onClick={onClick}>Clicked {count} times</button>
+      <br />
+    </>
+  );
+}
 function Footer() {
-    return (
-        <footer>
-            <Btn />
-            <br />
-            <Btn />
-        </footer>
-    )
+  const [count, setCount] = useState(0);
+  const clickedInTwice = () => {
+    setCount((count) => count + 1);
+  };
+  return (
+    <footer>
+      <Btn />
+      <Btn />
+      <SharingState count={count} onClick={clickedInTwice}/>
+      <SharingState count={count * 2} onClick={clickedInTwice}/>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
