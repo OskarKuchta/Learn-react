58f4769f27b1c51c625aa2f4c1921cb86a8aa85b
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-19 21:10:26 +0200

    Change component directory and text for btn

---
+++ app/src/components/greetings.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Button from "./Button";
+const Greetings = (props) => {
+  const [name, setName] = useState("");
+  const showGreetings = () => {
+    const typedName = document.querySelector(".nameValue");
+    const greetings = `${props.greets} ${typedName.value} ${props.lastTxt}`;
+    if (typedName.value != "" && typedName.value.match(/^[a-zA-Z]+$/)) {
+      setName(greetings);
+    } else {
+      typedName.classList.add("invalid");
+      setName("");
+    }
+  };
+  const clearInvalid = () => {
+    const typedName = document.querySelector(".nameValue");
+    typedName.classList.remove("invalid");
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        className="nameValue"
+        placeholder="Type your name"
+        required
+        onChange={clearInvalid}
+      />
+      <br />
+      <Button onClick={showGreetings} className="btn blue">
+        Login
+      </Button>
+      <br />
+      { name ? 
+        <p className="greetings-output">{name}</p>
+      : null}
+    </div>
+  );
+};
+export default Greetings;
--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -1,40 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
-
-const Greetings = (props) => {
-  const [name, setName] = useState("");
-  const showGreetings = () => {
-    const typedName = document.querySelector(".nameValue");
-    const greetings = `${props.greets} ${typedName.value} ${props.lastTxt}`;
-    if (typedName.value != "" && typedName.value.match(/^[a-zA-Z]+$/)) {
-      setName(greetings);
-    } else {
-      typedName.classList.add("invalid");
-      setName("");
-    }
-  };
-  const clearInvalid = () => {
-    const typedName = document.querySelector(".nameValue");
-    typedName.classList.remove("invalid");
-  };
-  return (
-    <div className="greetings">
-      <input
-        type="text"
-        className="nameValue"
-        placeholder="Type your name"
-        required
-        onChange={clearInvalid}
-      />
-      <br />
-      <Button onClick={showGreetings} className="btn blue">
-        Login
-      </Button>
-      <br />
-      <p className="greetings-output">{name}</p>
-    </div>
-  );
-};
+import Greetings from "./greetings";
 const Login = () => {
   const [checkstatus, setCheckstatus] = useState(true);
   const switchState = () => {
